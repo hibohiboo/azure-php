dceeeb39b3700ca04ea140c5b331fb61645118ac
hibohiboo <swdice66@gmail.com>
2021-07-24 19:04:48 +0900

    fix semi rule AB#89

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'prettier/prettier': ['off'],
-    'quotes': ['error', 'single']
+    'quotes': ['error', 'single'],
+    'semi': ['error']
   },
   overrides: [
     {
